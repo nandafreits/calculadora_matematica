01a145a9ab8ccc102f22c7f07b322d66287047d6
nandafreits <mfernandsf@gmail.com>
2024-09-12 20:22:34 -0300

    12/09

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,11 @@
-$(document).ready(function)() {
+$(document).ready(function() {
 
-    $('bt-nr').click 
-}
+    $('bt-nr').click(function() {
+        var nr_c = ' ';
+     bt_nr = $(this).text();
+    
+    $('#txt_display').val(bt_nr);
+
+    });
+
+});
