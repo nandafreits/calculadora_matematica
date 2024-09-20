1edb6fa14ed98e0983f6cde349f798f9ace56b98
nandafreits <mfernandsf@gmail.com>
2024-09-20 19:16:39 -0300

    20/09

--- js/script.js
+++ js/script.js
@@ -1,34 +1,14 @@
 $(document).ready(function() {
-        
+
+    $('txt_display').mask('000\'000\'000\'000\'000', {reverse: true});
     var bt_nr = '';
-    
-    $('.bt-nr').click(function(){
+
+    $('.bt-nr').click(function() {
         var n = $(this).text();
 
-        if(bt_nr[0] !== '0') {
-            
-            bt_nr = bt_nr.concat(n);
-        
-        } else if (bt_nr[0] !== '0' && bt_nr[1] !== '.') {
+        bt_nr = bt_nr.concat(n);
 
-            bt_nr = bt_nr.concat(n);
-
-        } else {
-            
-            if(bt_nr.length == 1 && n == '.') {
-
-                bt_nr = bt_nr.concat(n);
-                $(this).attr('disabled', 'true');
-
-            } else {
-
-                bt_nr = bt_nr.concat(n);
-
-            }
-        }
-
-        $('#txt_dispaly').val(bt_nr);
+        $('#txt_dusplay').val(bt_nr);
     });
-
 });
     
